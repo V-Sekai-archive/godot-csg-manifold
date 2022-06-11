f5aa96f280899c2515285d51c4d51581cb29567e
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-06-10 23:52:01 -0700

    Checkout.

---
+++ checkout.sh
@@ -0,0 +1,6 @@
+rm -rf ../../thirdparty/manifold
+
+degit github:fire/manifold-1.git#godot-manifold-04 ../../thirdparty/manifold
+degit github:fire/thrust#1.17.X ../../thirdparty/manifold/third_party/thrust
+degit github:g-truc/glm ../../thirdparty/manifold/third_party/glm
+degit github:google/googletest ../../thirdparty/manifold/third_party/google_test
---
+++ global-npm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir "${HOME}/.npm-packages"
+npm config set prefix "\${HOME}/.npm-packages"
+
+cat >> ~/.bashrc <<EOL
+NPM_PACKAGES="\${HOME}/.npm-packages"
+
+export PATH="\$PATH:$NPM_PACKAGES/bin"
+
+# Preserve MANPATH if you already defined it somewhere in your config.
+# Otherwise, fall back to `manpath` so we can inherit from `/etc/manpath`.
+export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man" 
+EOL
+
+
+
